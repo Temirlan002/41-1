5ddeb4a767e23be60f134322b3d7b5bfe0a9bd39
Tima19 <temirlan.baltabaev6@gmail.com>
2024-11-04 18:58:43 +0600

    lesson2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainPage from './pages/mainPage/MainPage';
+import Todo from './pages/todoPage/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MainPage /> */}
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
